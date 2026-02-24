51f05ef0b1b18e9b5f7b8b6cd68500c49fcab0e8
Luan Fontinele <luanfontinele18@gmail.com>
2026-02-24 00:46:25 -0300

    style: visual estatico finalizado

--- resources/views/livewire/Pages/Dashboard.blade.php
+++ resources/views/livewire/Pages/Dashboard.blade.php
@@ -1,14 +1,38 @@
 <div>
-    <h1 class="text-3xl text-[#2E4399] font-sans font-bold mb-24">Dashboard</h1>
+    <h1 class="text-3xl text-[#2E4399] font-sans font-bold mb-10">Dashboard</h1>
+
+    <div class="flex justify-between mb-10">
+        <div class="bg-white w-60 h-20 rounded">
+
+        </div>
+        <div class="bg-white w-60 h-20 rounded">
+
+        </div>
+        <div class="bg-white w-60 h-20 rounded">
+
+        </div>
+        <div class="bg-white w-60 h-20 rounded">
+
+        </div>
+    </div>
+
+    <div class="flex justify-between mb-10">
+        <div class="bg-white w-90 h-50 rounded">
+
+        </div>
+        <div class="bg-white w-90 h-50 rounded">
+
+        </div>
+        <div class="bg-white w-90 h-50 rounded">
+
+        </div>
+    </div>
 
     <div class="flex justify-between">
-        <div class="bg-white w-90 h-60 rounded">
+        <div class="bg-white w-180 h-80 rounded">
 
         </div>
-        <div class="bg-white w-90 h-60 rounded">
-
-        </div>
-        <div class="bg-white w-90 h-60 rounded">
+        <div class="bg-white w-130 h-80 rounded">
 
         </div>
     </div>
